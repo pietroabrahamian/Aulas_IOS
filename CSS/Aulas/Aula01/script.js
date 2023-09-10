2c625ed8536dafda30b82c560b9078725135e672
Pietro <pietroabrahamian2018@gmail.com>
2023-09-10 20:41:13 -0300

    icon + js

---
+++ CSS/Aulas/Aula01/script.js
@@ -0,0 +1,15 @@
+function scrollToElement (elementSelector, instance =0) {
+    const elements = document.querySelectorAll (elementSelector);
+    if (elements.length > instance) {
+        elements[instance].scrollIntoView({behavior: 'smooth'});
+    }
+    
+}
+
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+const link3 = document.getElementById("link3");
+
+link1.addEventListener('click', () =>{
+    scrollToElement('.tipo-habilidade')
+})

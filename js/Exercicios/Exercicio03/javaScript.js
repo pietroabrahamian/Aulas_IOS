037946d1ae785a6088e308db6206d8a85312ad37
Pietro <pietroabrahamian2018@gmailcom>
2023-10-17 18:29:31 -0300

    js3

--- js/Exercicios/Exercicio03/javaScript.js
+++ js/Exercicios/Exercicio03/javaScript.js
@@ -1,5 +1,10 @@
 let fruta1 = "Banana"
 let fruta2 = "Uva"
-let fruta3 = "Maça"
-let fruta4 = "Manga"
-let fruta5 = "  Melancia"
+let fruta3 = "      Maça     "
+let fruta4 = "Manga".replace('Manga', 'Maracuja')
+let fruta5 = "Melancia"
+console.log(fruta1.length);
+console.log(fruta2.substring(1,4));
+console.log(fruta3.trim());
+console.log(fruta4.toLowerCase());
+console.log(`A melhor fruta é ${fruta2}`);

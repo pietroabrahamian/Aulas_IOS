8df88361c653f10cbbe73b7b3674a18556608c75
Pietro <pietroabrahamian2018@gmailcom>
2023-10-19 21:46:30 -0300

    aula07

---
+++ js/Aula07/anotacoesArrayObjetos.js
@@ -0,0 +1,24 @@
+//  Array de Objetos
+// .stringify() --> muda do objeto para string para o JSON
+// .parse --> muda de string do JSON para objeto
+
+
+// OBJETO
+let carro = {
+    tipo:"Fiat",
+    modelo:"500",
+    cor:"branco"
+}
+console.log(carro);
+
+// ARRAY COM OBJETO
+let moto = [
+{
+    nome:"Pietro",
+    sobrenome:"Abrahamian",
+    idade:16
+},
+{
+    texto:"Encontrar o chefe"
+},]
+console.log(moto.stringify());
---
+++ js/Exercicios/Exercicio07/main.js
@@ -0,0 +1,47 @@
+let alunos = [{
+    nome1: "Pietro",
+    sobrenome: "Abrahamian",
+    notas: [10, 4, 9, 5],
+    diciplina:"Matematica",
+    endereco: {
+        rua: "Das Peras",
+        bairro: "Tucuruvi",
+        num: "78"
+    },
+    nacionalidade: "Espanhol",
+    nomePais: ["Jeferson", "Leila"],
+    faltas: 2,
+},
+{
+    nome: "Kaiky",
+    sobrenome: "Oliveira",
+    notas: [5, 7, 9, 10],
+    diciplina:"Matematica",
+    endereco: {
+        rua: "Das Maças",
+        bairro: "Cachoeirinha",
+        num: "70"
+    },
+    nacionalidade: "Espanhol",
+    nomePais: ["Jose", "Ana"],
+    faltas: 2,
+},
+{
+    nome: "Benjamim",
+    sobrenome: "Dias",
+    notas: [10, 9, 8, 10],
+    diciplina:"Matematica",
+    endereco: {
+        rua: "Das Morango",
+        bairro: "Cachoeirinha",
+        num: "19"
+    },
+    nacionalidade: "Espanhol",
+    nomePais: ["Carlos", "Josefa"],
+    faltas: 0,
+}
+];
+for (let media = alunos; media = notas; media) {
+    const element = array[index];
+    
+}

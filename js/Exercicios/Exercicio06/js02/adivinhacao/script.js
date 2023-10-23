414328f41108eea325dc4e168236b586be421485
Pietro <pietroabrahamian2018@gmail.com>
2023-10-22 22:36:11 -0300

    atualização ex: 7

--- js/Exercicios/Exercicio06/js02/adivinhacao/script.js
+++ js/Exercicios/Exercicio06/js02/adivinhacao/script.js
@@ -1,6 +1,18 @@
-let num = Number(window.prompt("Tente acertar um número de 1 a 10"))
+const numeroCerto = 4
+const num = window.prompt("Tente acertar um número de 1 a 10")
+    if (num == 4) {
+        window.alert("Parabéns, você acertou!")
+    } else {
+        window.alert("Tente Novamente!")
+        num
+    }
 
-while (num != 4) {
-    
 
-}
+
+
+
+
+
+
+
+

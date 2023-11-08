e108cadb604efedbef666a9c19e53df2eaf40216
Pietro <pietroabrahamian2018@gmailcom>
2023-11-07 21:40:42 -0300

    aula14

---
+++ js/Exercicios/Exercicio14/main.js
@@ -0,0 +1,10 @@
+let form = document.getElementById('addForm');
+let itemList = document.getElementById('items');
+
+form.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+
+function addItem(e) {
+    e.preventDefault()
+    let newItem = 
+}

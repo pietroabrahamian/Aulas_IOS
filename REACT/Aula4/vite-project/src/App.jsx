cbcbfa36bbb359ed74d8516d93f4e8ce2caefd56
Pietro <pietroabrahamian2018@gmailcom>
2023-11-16 20:27:55 -0300

    aula04-react

---
+++ REACT/Aula4/vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import { Component } from 'react';
+import Exemplo from './Components/Exemplo';
+import Header from './Components/Header';
+
+export class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Exemplo />
+        <Header />  
+      </div>
+    );
+  }
+}
+
+export default App;
+
